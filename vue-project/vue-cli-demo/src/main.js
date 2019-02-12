6d6eb96a17ee87551b452dd6a258d86c21c96181
Flying_fish <1371545842@qq.com>
2019-02-12 19:44:53 +0800

    更改

--- vue-project/vue-cli-demo/src/main.js
+++ vue-project/vue-cli-demo/src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// import VueResource from 'vue-resource'
+// Vue.use(VueResource)
+
 // css reset  css初始化
 import './styles/common.css'
 // 引入mui的css文件
@@ -24,5 +27,5 @@ new Vue({
   el: '#app',
   router,
   // render函数的作用是将APP组件替换掉#app盒子
-  render: h => h(app)
+  // render: h => h(app)
 })
