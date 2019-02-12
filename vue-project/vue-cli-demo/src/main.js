b13f906137d94e2ab7bc24210ef7fb2e6685e71d
Flying_fish <1371545842@qq.com>
2019-02-12 19:43:37 +0800

    更改

--- vue-project/vue-cli-demo/src/main.js
+++ vue-project/vue-cli-demo/src/main.js
@@ -4,12 +4,25 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// css reset  css初始化
+import './styles/common.css'
+// 引入mui的css文件
+import './libs/mui/css/mui.css'
+// 引入mui扩展字体的css文件
+import './libs/mui/css/icons-extra.css'
+
+//导入mint-ui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+Vue.use(MintUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  // render函数的作用是将APP组件替换掉#app盒子
+  render: h => h(app)
 })
