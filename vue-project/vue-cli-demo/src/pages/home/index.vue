83ac961a109315b09b1dc63ffffa9270b8bc4972
Flying_fish <1371545842@qq.com>
2019-02-13 08:48:04 +0800

    更改

--- vue-project/vue-cli-demo/src/pages/home/index.vue
+++ vue-project/vue-cli-demo/src/pages/home/index.vue
@@ -9,39 +9,39 @@
             </mt-swipe-item>
         </mt-swipe>
 
-        <!-- 九宫格 -->
+        <!-- 九宫格盒子 -->
         <ul class="mui-table-view mui-grid-view mui-grid-9">
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4">
                 <a href="#">
                 <img src="@/assets/menu1.png" alt="">
                 <div class="mui-media-body">新闻资讯</div>
                 </a>
             </li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4">
                 <a href="#">
                 <img src="@/assets/menu2.png" alt="">
                 <div class="mui-media-body">图片分享</div>
                 </a>
             </li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4">
                 <a href="#">
                 <img src="@/assets/menu3.png" alt="">
                 <div class="mui-media-body">商品购买</div>
                 </a>
             </li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4">
                 <a href="#">
                 <img src="@/assets/menu4.png" alt="">
                 <div class="mui-media-body">留言反馈</div>
                 </a>
             </li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4">
                 <a href="#">
                 <img src="@/assets/menu5.png" alt="">
                 <div class="mui-media-body">视频专区</div>
                 </a>
             </li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4">
                 <a href="#">
                 <img src="@/assets/menu6.png" alt="">
                 <div class="mui-media-body">联系我们</div>
